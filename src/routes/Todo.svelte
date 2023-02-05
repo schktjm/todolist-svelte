82d57791acaf8b2582a32413ba989dcfe74e846b
schktjm <schktjm@gmail.com>
2023-02-05 19:57:10 +0900

    :sparkles: 基礎項目作成

---
+++ src/routes/Todo.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { Todo } from './type';
+
+	export let status: Todo['status'];
+	export let text: Todo['text'];
+</script>
+
+<div>
+	<input type="checkbox" checked={status === 'done'} />
+	{text}
+</div>
---
+++ src/routes/Todolist.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import Todo from './Todo.svelte';
+	import type { Todo as TodoType } from './type';
+
+	const todos: TodoType[] = [
+		{
+			id: '1',
+			status: 'todo',
+			text: '風呂掃除'
+		},
+		{
+			id: '2',
+			status: 'done',
+			text: 'お皿洗い'
+		},
+		{
+			id: '3',
+			status: 'done',
+			text: '掃除機かけ'
+		},
+		{
+			id: '4',
+			status: 'todo',
+			text: '洗濯物干す'
+		}
+	];
+
+	let isHideDone = true;
+</script>
+
+<label>
+	<input type="checkbox" bind:checked={isHideDone} />
+	完了済みの項目を隠す
+</label>
+
+<ul>
+	{#each todos as todo (todo.id)}
+		{#if !(isHideDone && todo.status === 'done')}
+			<Todo {...todo} />
+		{/if}
+	{/each}
+</ul>
