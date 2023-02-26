170e0c204f45b20b9ffa785ca9e212798a95bd99
schktjm <schktjm@gmail.com>
2023-02-26 23:17:09 +0900

    :art: global css を整え

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,12 @@
 	import './styles.css';
 </script>
 
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
+	<link href="https://fonts.googleapis.com/css2?family=Jua&display=swap" rel="stylesheet" />
+</svelte:head>
+
 <div class="app">
 	<main>
 		<slot />
